acd00c6846787346a393f58ef4db63adb7faf2e9
lobna3 <yousfi55lobna@gmail.com>
2022-05-18 10:45:37 +0100

    message

--- controllers/commandes.js
+++ controllers/commandes.js
@@ -331,7 +331,7 @@ const generateInvoice = async (req, res) => {
 
 
 const testEmail = async (req, res)=>{
-  await gmail_mailer(req.body.email, req.body.subject, req.body. description)
+  await gmail_mailer(req.body.email, req.body.subject, req.body.description)
   res.status(200).json({success: true})
 }
 
